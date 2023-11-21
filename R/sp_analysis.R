efc58d80a8022af14be296432cfbee12a9b67a75
youyupei <youyupei@gmail.com>
2023-11-21 12:40:53 +1100

    add gene id to gene name conversion and mito gene perc function

--- R/sp_analysis.R
+++ R/sp_analysis.R
@@ -4,8 +4,7 @@
   library(ggplot2)
   library(Matrix)
   library(jsonlite)
-  
-
+  library(biomaRt)
 
   create_spe <- function(gene_count_file, full_list, trans_count=NULL, mannual_align_json=NULL, image_file=NULL) {
     
@@ -131,7 +130,25 @@ merge_dfs <- function(df1, df2, ...) {
 
 
 
-plot_visium_spot <- function(flames_rst, level, annotatio=NULL, image=FALSE ...){
+#' Plot Visium Spot
+#'
+#' This function plots the spots in a Visium dataset at a specified level of analysis.
+#'
+#' @param flames_rst The FLAMES result object.
+#' @param level The level of analysis ('gene', 'transcript'/'isoform').
+#' @param annotation Optional annotation column to color the spots.
+#' @param image Logical indicating whether to overlay an image as the background.
+#' @param ... Additional arguments to be passed to ggplot.
+#'
+#' @return A ggplot object representing the plot of Visium spots.
+#'
+#' @examples
+#' # Plot Visium spots at the gene level
+#' plot_visium_spot(flames_rst, level = 'gene')
+#'
+#' # Plot Visium spots at the transcript level with an annotation column
+#' plot_visium_spot(flames_rst, level = 'transcript', annotation = 'cluster')
+plot_visium_spot <- function(flames_rst, level, annotation=NULL, image=FALSE, ...){
   if (image) {
     background_img <- flames_rst$image
   }
@@ -146,8 +163,8 @@ plot_visium_spot <- function(flames_rst, level, annotatio=NULL, image=FALSE ...)
 
   plot_data <- as.data.frame(spatialCoords(spe))
 
-  if (!is.null(annotatio)) {
-    plot_data[[annotatio]] <- colData(spe)[[annotatio]]
+  if (!is.null(annotation)) {
+    plot_data[[annotation]] <- colData(spe)[[annotation]]
   }
 
   if (image) {
@@ -161,11 +178,103 @@ plot_visium_spot <- function(flames_rst, level, annotatio=NULL, image=FALSE ...)
   p <- p + 
     geom_point(
       data=plot_data, 
-      aes(x=imageX, y=maxY-imageY, colour = !!as.name(annotation)), alpha=0.4, size = 0.8) + 
+      aes(x=imageX, y=maxY-imageY, colour = !!as.name(annotation)), alpha=ifelse(image, 0.9,1), size = 0.8) + 
     xlim(1, maxX) +
     ylim(1, maxY) +
     coord_fixed() + 
+    scale_color_viridis_c() +
     theme_void()
   
   return(p)
 }
+
+QC_filter <- function(flames_rst, filter_func, level = 'both', ...) {
+  if (level == 'both') {
+      flames_rst$gene_spe <- filter_func(flames_rst$gene_spe, ...)
+      flames_rst$trans_spe <- filter_func(flames_rst$trans_spe, ...)
+  } else if (level == 'gene') {
+    flames_rst$gene_spe <- filter_func(flames_rst$gene_spe, ...)
+  } else if (level %in% c('transcript', 'isoform')) {
+    flames_rst$trans_spe <- filter_func(flames_rst$trans_spe, ...)
+  } else {
+    stop("level must be one of 'gene', 'transcript'")
+  }
+  return(flames_rst)
+}
+
+
+#' Convert Ensembl gene IDs to gene names
+#'
+#' This function takes a FLAMES result object and an Ensembl dataset name as input.
+#' It converts the Ensembl gene IDs in the FLAMES result object to gene names using the specified Ensembl dataset.
+#' The function retrieves the gene names from the Ensembl Biomart database.
+#' The converted gene names are added as a new column in the FLAMES result object.
+#'
+#' @param flames_rst A FLAMES result object.
+#' @param ensembl_dataset The name of the Ensembl dataset to use for gene ID conversion.
+#' To see the datasets available within a biomaRt you can do with biomaRt, e.g., \code{mart = useMart('ensembl')}, followed by \code{listDatasets(mart)}.
+#'
+#' @return The FLAMES result object with the Ensembl gene IDs converted to gene names.
+#'
+#' @examples
+#' flames_rst <- convert_ensembl_gene_id(flames_rst, "hsapiens_gene_ensembl")
+#' print(flames_rst)
+#'
+#' @importFrom biomaRt useMart getBM
+#' @importFrom dplyr select
+#' @importFrom stringr sub
+#' @importFrom utils merge
+#' @importFrom utils match
+#' @importFrom utils cbind
+#' @importFrom utils colnames
+#' @importFrom utils rownames
+#' @importFrom utils rowData
+#' @importFrom utils merge_dfs
+convert_ensembl_gene_id <- function(flames_rst, ensembl_dataset) {
+  mart <- useMart(biomart = "ensembl", dataset=ensembl_dataset)
+  gene_id <- rownames(flames_rst$gene_spe)
+  rowData(flames_rst$gene_spe) <- cbind(rowData(flames_rst$gene_spe), gene_id)
+  gene_id <- sub("\\..*$", '', gene_id)
+
+  genes_info <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
+                      filters = "ensembl_gene_id", values = gene_id, mart = mart)
+  
+  colnames(genes_info) <- c('gene_id', 'gene_name')
+
+  genes_info <- genes_info[match(gene_id, genes_info$ensembl_gene_id),]
+  rowData(flames_rst$gene_spe) <- cbind(rowData(flames_rst$gene_spe), genes_info)
+  rowData(flames_rst$gene_spe) <- merge_dfs(genes_info, rowData(flames_rst$gene_spe), by='gene_id')
+
+  return(flames_rst)
+}
+
+
+#' Calculate the percentage of mitochondrial genes in a FLAMES result object
+#'
+#' This function calculates the percentage of mitochondrial genes in a FLAMES result object.
+#' It first checks if the "gene_name" column is present in the gene_spe slot of the FLAMES result object.
+#' If not found, it throws an error and suggests running the "convert_ensembl_gene_id" function first.
+#' Then, it identifies the mitochondrial genes based on their gene names starting with "mt-".
+#' Finally, it calculates the sum of expression values for mitochondrial genes and divides it by the sum of expression values for all genes.
+#'
+#' @param flames_rst A FLAMES result object
+#' @return A numeric vector representing the percentage of mitochondrial genes
+#' @examples
+#' flames_rst <- create_flames_result(flames_rst, "mmusculus_gene_ensembl")
+#' get_mito_gene_perc(flames_rst)
+#' @export
+get_mito_gene_perc <- function(flames_rst) {
+  if (any(names(colData(flames_rst$gene_spe)) == "mito_perc")) {
+    stop("mito_perc column already exists in gene_spe.")
+  }
+  if (!any(names(rowData(flames_rst$gene_spe)) == "gene_name")) {
+    stop("gene_name column not found in gene_spe, please run convert_ensembl_gene_id first.")
+  }
+
+  # get sum of rows for each gene
+  is_mito <- rowData(flames_rst$gene_spe)$gene_name %>% grepl('^mt-',.)
+  mito_perc <- colSums(assay(flames_rst$gene_spe[is_mito,]))/colSums(assay(flames_rst$gene_spe))
+  colData(flames_rst$gene_spe) <- cbind(colData(flames_rst$gene_spe), mito_perc)
+  return(flames_rst)
+}
+
